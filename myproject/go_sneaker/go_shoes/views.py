a307d32f5f11ba1785bb16d45319ad4c211a4ab8
Minh Quân IT A01 <76775924+minhquanita01@users.noreply.github.com>
2023-11-02 10:11:22 +0700

    Adding_files

---
+++ myproject/go_sneaker/go_shoes/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from go_shoes.models import GO_Shoes
+import json
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from django.template.loader import render_to_string
+
+@require_POST
+@csrf_exempt
+def add_to_cart(request):
+    data = json.loads(request.body)
+    shoe_id = data.get('shoe_id')
+    try:
+        shoe = GO_Shoes.objects.get(shoesID = shoe_id)
+    except GO_Shoes.DoesNotExist:
+        return JsonResponse({'error': 'Shoe not found'}, status=404)
+    
+    cart = request.session.get('cart', {})
+    request.session['cart'] = cart
+    
+    total_cost = sum(GO_Shoes.objects.get(shoesID=id).shoes_price for id in cart)
+    
+    cart_item_html = render_to_string('cart_item.html', {'shoe': shoe})
+    
+    return JsonResponse({'html': cart_item_html, 'total_cost': total_cost})
+
+def index(request):
+    #Fetch only products which has quantity greater than 0 (it means that these products still available in the store)
+    available_shoes = GO_Shoes.objects.exclude(shoes_quantity = 0).values
+    (
+        'shoes_image_path', 
+        'shoes_name', 
+        'shoes_description', 
+        'shoes_price', 
+        'shoes_color'
+    )
+
+    context = {'available_shoes': available_shoes}
+    return render(request, 'index.html', context)
