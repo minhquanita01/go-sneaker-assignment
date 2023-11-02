a11ee86782da08b513ca96b987d798e12bbfa3f1
Minh Quan IT A01 <minhquan069.fit.hcmus@gmail.com>
2023-11-02 11:43:19 +0700

    Update

--- go_sneaker/go_shoes/admin.py
+++ go_sneaker/go_shoes/admin.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 
 # Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> 0d41086a3a90370f94e40901e573e9ddf10d20c3
--- go_sneaker/go_sneaker/urls.py
+++ go_sneaker/go_sneaker/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 URL configuration for go_sneaker project.
 
@@ -24,30 +23,3 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('go_shoes/add-to-cart/', views.add_to_cart, name='add-to-cart'),
 ]
-=======
-"""
-URL configuration for go_sneaker project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from go_shoes import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('go_shoes/add-to-cart/', views.add_to_cart, name='add-to-cart'),
-]
->>>>>>> 0d41086a3a90370f94e40901e573e9ddf10d20c3
