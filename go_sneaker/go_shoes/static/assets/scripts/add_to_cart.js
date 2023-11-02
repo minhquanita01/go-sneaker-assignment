167a57a21e4c6e0178684f88b11b998064f84f06
Minh Quan IT A01 <minhquan069.fit.hcmus@gmail.com>
2023-11-02 11:46:03 +0700

    Update

--- go_sneaker/go_shoes/static/assets/scripts/add_to_cart.js
+++ go_sneaker/go_shoes/static/assets/scripts/add_to_cart.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function() 
 {
     var addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
@@ -36,42 +35,3 @@ function updateCartDisplay(data)
 
     document.querySelector(".cart .total-cost").textContent = `$${data.total_cost}`;
 }
-=======
-document.addEventListener('DOMContentLoaded', function() 
-{
-    var addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
-    
-    addToCartButtons.forEach(function(button) 
-    {
-        button.addEventListener('click', function() 
-        {
-            var shoeId = this.getAttribute('data-shoe-id');
-
-            this.style.display = 'none';
-            this.nextElementSibling.style.display = 'flex';
-            
-            fetch('/go_shoes/add-to-cart/', 
-            {
-                method: 'POST',
-                body: JSON.stringify({ 'shoe_id': shoeId }),
-                headers: 
-                {
-                    'Content-Type': 'application/json',
-                }
-            })
-            .then((response) => response.json())
-            .then(function(data) 
-            {
-                updateCartDisplay(data);
-            });
-        });
-    });
-});
-
-function updateCartDisplay(data) 
-{
-    document.querySelector('.cart.column-body').innerHTML += data.html;
-
-    document.querySelector(".cart .total-cost").textContent = `$${data.total_cost}`;
-}
->>>>>>> 0d41086a3a90370f94e40901e573e9ddf10d20c3
